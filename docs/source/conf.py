f6d2944db8ebd6c19caa4399354587fd8273b23b
JonathanGrossman <jon@jonegrossman.com>
2022-03-20 15:22:25 +0200

    change sidebar toc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,3 +35,5 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 master_doc = "contents"
+
+html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
